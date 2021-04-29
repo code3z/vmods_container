aff69ed5ecf2ebc258100f834b9226e3c735ed4a
code3z <code3@vivaldi.net>
2021-04-29 15:33:49 -0400

    Add basic update files

---
+++ forceupdate.sh
@@ -0,0 +1,3 @@
+rm -rf vivaldi_mods_container
+git clone https://github.com/code3z/vivaldi_mods_collection.git
+./updatemods.sh
---
+++ updatemods.sh
@@ -0,0 +1,2 @@
+cp custom vivaldi_mods_collection/custom
+python3 vivaldi_mods_collection/custom.py -i config.json
